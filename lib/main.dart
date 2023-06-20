ac5f6c676e039ec4688870ea7036f7b548a9208e
NasciGN <gnascimento.job@gmail.com>
2023-06-20 14:48:06 -0400

    1.7 - Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-
 import 'package:pokedex/views/pages/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'modelsviews/firebase/firebase_options.dart';
 
-void main() {
-  runApp(MaterialApp(
+Future<void> main() async {
+  /* WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); */
+  runApp(GetMaterialApp(
     initialRoute: '/home',
-    routes: {
-      '/home': (_) => const HomeScreen(),
-    },
+    getPages: [
+      GetPage(name: '/home', page: () => const HomeScreen()),
+    ],
   ));
 }
